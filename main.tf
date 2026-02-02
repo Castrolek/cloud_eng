847f7264e9d2f868f34dbbfb162d10d004cdc762
Castrolek <castrolek1@gmail.com>
2026-02-02 23:22:36 +0100

    Added auto pipeline

--- main.tf
+++ main.tf
@@ -1,124 +1,61 @@
 provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true 
+  region = "eu-central-1" 
+}
 
-  endpoints {
-    s3 = "http://localhost:4566"
-    iam = "http://localhost:4566"
-    sts = "http://localhost:4566"
-    ec2 = "http://localhost:4566"
+# 1. Tworzymy koszyk S3
+resource "aws_s3_bucket" "moja_strona" {
+  bucket = "moje-unikalne-portfolio-2026-xyz" # Musi być unikalna nazwa na świecie!
+}
+
+# 2. Konfigurujemy go jako stronę WWW
+resource "aws_s3_bucket_website_configuration" "config" {
+  bucket = aws_s3_bucket.moja_strona.id
+
+  index_document {
+    suffix = "index.html"
   }
 }
 
-resource "aws_s3_bucket" "testowy_bucket_nowy" {
-  bucket = "unikalna-nazwa-12345"
+# 3. Wyłączamy blokady publicznego dostępu (Security!)
+resource "aws_s3_bucket_public_access_block" "dostep" {
+  bucket = aws_s3_bucket.moja_strona.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_public_access_block" "security_policy" {
-  bucket = aws_s3_bucket.testowy_bucket_nowy.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+# 4. Dodajemy prosty plik HTML
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.moja_strona.id
+  key          = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+  etag = filemd5("index.html")
 }
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.testowy_bucket_nowy.id
-  rule{
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
+resource "aws_s3_object" "style" {
+  bucket = aws_s3_bucket.moja_strona.id
+  key = "style.css"
+  source = "style.css"
+  content_type = "text/css"
+  etag = filemd5("style.css")
   
 }
 
-resource "aws_iam_user" "junior_dev" {
-  name="Junior_dev_poznan"
-}
-
-resource "aws_iam_policy" "read_only_buckets" {
-  name = "ReadOnlyS3Buckets"
-  description = "Pozwala tylko na podgląd listy"
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.moja_strona.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = [
-          "s3:ListAllMyBuckets",
-          "s3:GetBucketLocation"
-        ]
-        Effect = "Allow"
-        Resource = "*"
-      }
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.moja_strona.arn}/*"
+      },
     ]
   })
-  
-}
-resource "aws_iam_user_policy_attachment" "dev_attach" {
-  user = aws_iam_user.junior_dev.name
-  policy_arn = aws_iam_policy.read_only_buckets.arn
-  
-}
-
-resource "aws_vpc" "glowna_siec" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-
-  tags={
-    Name = "VPC-Poznan-Lab"
-  }
-}
-resource "aws_subnet" "Podsiec_prywatna" {
-  vpc_id = aws_vpc.glowna_siec.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "Private_subnet"
-  }
-  
-}
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  description = "Pozwol na bezpieczny ruch WWW"
-  vpc_id      = aws_vpc.glowna_siec.id
-
-  ingress {
-    description = "HTTPS od swiata"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Web-Firewall"
-  }
-}
-
-resource "aws_instance" "serwer_bezpieczny" {
-  ami           = "ami-ff0fea31"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.Podsiec_prywatna.id
-
-
-  vpc_security_group_ids = [aws_security_group.allow_web.id]
-
-  tags = {
-    Name = "Serwer-Bastion-Poznan"
-  }
 }
